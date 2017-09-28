2b964fb02cf0c4873f973e1819dada9b14abe9c3
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2017-09-28 07:41:44 +0100

    Initial Commit

---
+++ CStructures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CStructures
+//
+//  Created by Nicholas Outram on 28/09/2017.
+//  Copyright © 2017 Nicholas Outram. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
